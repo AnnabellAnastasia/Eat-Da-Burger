8968c515492d2c4c49d4d6bbcc3c591e1be77b16
Your Name <annabellawilkins@Oss-MacBook-Pro.local>
2019-06-10 00:42:37 -0400

    firstroute

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,7 +2,9 @@
 var express = require('express');
 var router = express.Router();
 
-// create a rpute
+// create a route
 router.get('/', function(req, res){
     res.render('index');
 })
+
+module.exports = router;
