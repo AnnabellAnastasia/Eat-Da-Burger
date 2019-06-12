284f3c45e8e377f807f9d6a749ba6b22e06f8baf
Your Name <annabellawilkins@Oss-MacBook-Pro.local>
2019-06-12 17:16:28 -0400

    database

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers ( burger_name, devoured ) VALUES ( 'Cheese Burgers', 'FALSE');
+INSERT INTO burgers ( burger_name, devoured ) VALUES ( 'Hamburgers', 'FALSE');
+INSERT INTO burgers ( burger_name, devoured ) VALUES ( 'Pizza Burger', 'FALSE');
